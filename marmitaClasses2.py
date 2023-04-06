cd0668abe3363b572752a08b3164ba6487f6d8c8
Hidden-Warden <70717127+Hidden-Warden@users.noreply.github.com>
2023-04-06 08:59:42 +0200

    :fish_cake:

---
+++ marmitaClasses2.py
@@ -0,0 +1,55 @@
+import sqlite3
+
+class Recette():
+    def __init__(self,Id,nom,image,preparation,cuisson,nbpers,diff,ingredients,etapes,famille):
+        self.Id=Id
+        self.nom=nom
+        self.image=image
+        self.preparation=preparation
+        self.cuisson=cuisson
+        self.nombreDePersonnes=nbpers
+        self.difficulte=diff
+        self.ingredients=ingredients
+        self.etapes=etapes
+        self.famille=famille
+
+class Famille():
+    def __init__(self, Id,nom,image):
+        self.Id=Id
+        self.nom=nom
+        self.image=image
+
+class Ingredient():
+    def __init__(self,Id, nom, quantite,unite=None):
+        self.Id=Id
+        self.nom=nom
+        self.quantite=quantite
+        self.unite=unite
+
+class Etape():
+    def __init__(self,num,texte):
+        self.num=num
+        self.texte=texte
+
+
+def accueil():
+    #no input argument
+    #return name and image of all families of recipes
+    pass
+
+def famille(idFamille):
+    pass
+
+def recette(idRecette):
+    pass
+
+
+conn = sqlite3.connect('marmita.db')
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM famille")
+
+conn.commit()
+
+cur.close()
+conn.close()
